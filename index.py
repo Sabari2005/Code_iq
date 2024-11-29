287a9963f9bc705f29c58262c55f6c6b21d0eb71
UVARAJAN D <sabari2005clg@gmail.com>
2024-11-29 23:25:52 +0530

    files updated

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI, Request, File, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -23,7 +22,7 @@ async def index(request: Request):
 async def upload_file(file: UploadFile = File(...)):
     file_location = os.path.join(UPLOAD_DIRECTORY, file.filename)
     print(file.filename)
-
+    # Save the file to the upload directory
     with open(file_location, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
     async with httpx.AsyncClient() as client:
@@ -36,9 +35,21 @@ async def start_stream(request: Request):
     ip = data.get("ip")
     port = data.get("port")
 
+    # Forward IP and port to fastapi_server2
     async with httpx.AsyncClient() as client:
         response = await client.post("http://127.0.0.2:8001/send_ip", json={"ip": ip, "port": port})
     return {"status": "success"} if response.status_code == 200 else {"status": "failed"}
+
+@app.post("/start_text_description")
+async def start_description(request: Request):
+    data = await request.json()
+    content = data.get("content")
+
+    # Forward IP and port to fastapi_server2
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://127.0.0.2:8001/content", json={"content":content})
+    return {"status": "success"} if response.status_code == 200 else {"status": "failed"}
+
 
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000)
